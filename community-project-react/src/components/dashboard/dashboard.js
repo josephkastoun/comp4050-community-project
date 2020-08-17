a91184a0023ce8cb52fac287cd2a2635feacca18
lukemclean97 <luke.mclean@students.mq.edu.au>
2020-08-18 09:46:09 +1000

    dashboard test

---
+++ community-project-react/src/components/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+        <div>
+            <p>Hello World!</p>
+        </div>
+    );
+  }
+}
