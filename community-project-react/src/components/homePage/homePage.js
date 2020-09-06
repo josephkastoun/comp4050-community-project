ca4adfcbede9ae0d67dced81a0236880b5e7530d
Eric <sang.yoon1@students.mq.edu.au>
2020-09-06 21:11:58 +1000

    Integrated homePage with jobPage
    Integrated Hannes's homePage with my jobPage

--- community-project-react/src/components/homePage/homePage.js
+++ community-project-react/src/components/homePage/homePage.js
@@ -1,27 +1,100 @@
-import React from '../../../node_modules/react';
-import logo from '../../resources/homePage/logo.svg'
+import React, { Component , useState} from '../../../node_modules/react';
+import {
+    Link
+  } from "react-router-dom";
 import './homePage.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is a test edit!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: null,
+      jobs: [
+        { id: 1, title: 'Mow Lawn', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 30, loc: 'Macquarie Park',},
+        { id: 2, title: 'Fix Door', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 20, loc: 'Epping'},
+        { id: 3, title: 'Paint Wall', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 20, loc: 'Macquarie Park'},
+        { id: 4, title: 'Lift to Train Station', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 10, loc: 'Macquarie Park'},
+        { id: 5, title: 'Pack Boxes', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 35, loc: 'Epping'},
+        { id: 6, title: 'Walk Dogs', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 25, loc: 'Macquarie Park'},
+        { id: 7, title: 'Clean Gutters', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 40, loc: 'Lane Cove'},
+        { id: 9, title: 'Install/Setup New TV', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 15, loc: 'Macquarie Park'},
+        { id: 10, title: 'Wash Car', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 10, loc: 'Lane Cove'},
+        { id: 11, title: 'Vacuum Carpets', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 10, loc: 'Epping'},
+        { id: 12, title: 'Pickup Kids from School', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 20, loc: 'Chatswood'},
+        { id: 13, title: 'Remove Tree Branch', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 25, loc: 'Epping'}
+        
+      ]
+    };
+  }
+
+  handleSelect (e) {
+    console.log(e);
+    this.setState({location : e})
+  }
+  
+  render() {
+    var locations = this.state.jobs
+    var locat = this.state.location
+
+    if(locat != null) {
+      locations = this.state.jobs.filter(function (job) {
+        return job.loc === locat;
+      });
+    }
+
+    
+
+    let jobList = locations.map(job => {
+      return (
+        <Link to={{pathname: "/job", state: { job: job}}}>
+        <div className="card border-dark mb-3">
+          <div className="card-body text-dark">
+              <h5 className="card-title">{job.title}</h5>
+              <p className="card-text">{job.desc}</p>
+          </div>
+          <div className="card-footer bg-transparent border-dark">
+            <p className="jobLocation">Location: {job.loc}</p>
+            <p className="jobCost">Cost: {job.price}</p>
+          </div>
+        </div>
+        </Link>
+      );
+    })
+    
+    return (
+      <div className="container">
+        <DropdownButton
+          alignRight
+          title="Location"
+          id="dropdown-menu-align-right"
+          variant="secondary"
+          onSelect={val => this.handleSelect(val)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Dropdown.Item eventKey="">All</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="Macquarie Park">Macquarie Park</Dropdown.Item>
+          <Dropdown.Item eventKey="Epping">Epping</Dropdown.Item>
+          <Dropdown.Item eventKey="Lane Cove">Lane Cove</Dropdown.Item>
+          <Dropdown.Item eventKey="Chatswood">Chatswood</Dropdown.Item>
+        </DropdownButton>
+
+
+
+        <div className="row">
+            {jobList}
+        </div>
+
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
---
+++ community-project-react/src/components/jobPage/jobPage.js
@@ -0,0 +1,58 @@
+
+import React, { Component , useState} from '../../../node_modules/react';
+import './jobPage.css';import {
+    Link
+  } from "react-router-dom";
+
+class Job extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  handleSelect (e) {
+    console.log(e);
+    this.setState({location : e})
+  }
+
+  componentDidMount() {
+      console.log("This. ", this.props.location.state.job)
+  }
+  
+  render() {
+      const job = this.props.location.state.job
+      const seller = job.seller
+      return (
+        <div className="container">
+            <div className="descriptionContainer">
+
+                <img className="jobImage" src={"https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2522&q=80"} />
+                <div>
+                    <h3 className="jobTitle">
+                        {job.title}
+                    </h3>
+                    <h6>
+                        Location: {job.loc}
+                    </h6>
+                    <p>
+                        {job.desc}
+                    </p>
+                </div>
+            </div>
+            
+            <div className="sellerDetails">
+                <p className="sellerName">John Doe   <span className="sellerRating">4.5</span></p>
+            </div>
+
+            <Link to="/">
+                <div className="backButton">
+                    Go Back
+                </div>
+            </Link>
+        </div>
+    )}
+}
+
+export default Job;
