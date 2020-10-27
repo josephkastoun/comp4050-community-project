a827a815fda0a467c5cdc123cdff154fe5faf060
josephkastoun <48784795+josephkastoun@users.noreply.github.com>
2020-10-27 11:36:18 +1100

    search now works!

--- community-project-react/src/components/search.js
+++ community-project-react/src/components/search.js
@@ -20,31 +20,21 @@ class search extends Component {
             })
     }
 
-
-    rebuildIndex() {
-        const allJobs = this.props.jobs;
-
-        const dataToSearch  = new JsSearch.Search('jobs');
-
-        dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
-        dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer()
-        dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex("jobs")
-        
-        dataToSearch.addIndex('title');
-        dataToSearch.addIndex('description');
-        dataToSearch.addIndex('location');
-        dataToSearch.addIndex('jobs');
-
-        dataToSearch.addDocument(allJobs)
-        this.setState({ search: dataToSearch, isLoading: false })
-    }
-
-
     searchData = e => {
-        const { search } = this.state
-        const queryResult = search.search(e.target.value)
-        this.setState({ searchQuery: e.target.value, searchResults: queryResult })
+        const queryResult = e.target.value;
+        let data = []
+
+        this.state.jobs.forEach(e => {
+            if(e.description.toLowerCase().search(queryResult.toLowerCase()) != -1 
+            || e.title.toLowerCase().search(queryResult.toLowerCase()) != -1
+            || e.location.toLowerCase().search(queryResult.toLowerCase()) != -1){
+                data.push(e);
+            }
+        });
+
+        this.setState({ searchQuery: e.target.value, searchResults: data })
       }
+      
       handleSubmit = e => {
         e.preventDefault()
       }
