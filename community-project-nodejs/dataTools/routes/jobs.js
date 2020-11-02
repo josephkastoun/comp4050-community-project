aa74d46cc5fb65d98dfe81a5df3887e2009b8bea
yuchen45 <yu.lin6@students.mq.edu.au>
2020-11-02 12:22:49 +1100

    specified database name

--- community-project-nodejs/dataTools/routes/jobs.js
+++ community-project-nodejs/dataTools/routes/jobs.js
@@ -5,7 +5,7 @@ const { finished } = require('stream');
 const ObjectID = require('mongodb').ObjectID
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://testuser:1234@communityproject.7gya3.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://testuser:1234@communityproject.7gya3.mongodb.net/userData?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 
